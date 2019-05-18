aeb8a73087809e19ec557a3d15e94c2b0d38f610
Guilherme Mattos <45467253+guilhermemattos16@users.noreply.github.com>
2019-05-18 11:24:07 -0300

    Exercicio 15

---
+++ Exercicios 15/Data.java
@@ -0,0 +1,67 @@
+import java.util.Date;
+
+public class Data extends Date {
+
+	private static final long serialVersionUID = 6299565376922637204L;
+	private int mes;
+
+	public void setMonth(String _mes) {
+		_mes = _mes.toLowerCase();
+
+		if(_mes.equals("janeiro")) 
+			this.setMonth(1);
+		if(_mes.equals("fevereiro")) 
+			this.setMonth(2);
+		if(_mes.equals("março") || _mes.equals("marco")) 
+			this.setMonth(3);
+		if(_mes.equals("abril")) 
+			this.setMonth(4);
+		if(_mes.equals("maio")) 
+			this.setMonth(5);
+		if(_mes.equals("junho")) 
+			this.setMonth(6);
+		if(_mes.equals("julho")) 
+			this.setMonth(7);
+		if(_mes.equals("agosto")) 
+			this.setMonth(8);
+		if(_mes.equals("setembro")) 
+			this.setMonth(9);
+		if(_mes.equals("outubro")) 
+			this.setMonth(10);
+		if(_mes.equals("novembro")) 
+			this.setMonth(11);
+		if(_mes.equals("dezembro")) 
+			this.setMonth(12);
+		else 
+			this.setMonth(-1);
+	}
+	
+	@SuppressWarnings("deprecation")
+	public int getMonth() {
+		return super.getMonth() + 1;
+	}
+	
+	public long quantidadeDias(Data _aux) {
+		long quantidade = 0;
+		quantidade = ((this.getTime() - _aux.getTime()) / (1000 * 60 * 24));
+		
+		// Caso a quantidade de dias seja negativa, calcular o valor absoluto
+		if (quantidade < 0)
+			quantidade = (-1) * quantidade;
+		return quantidade;
+
+	}
+
+	public void incrementa(long _milisegundos) {
+		long dias;
+		dias = this.getTime() + _milisegundos;
+		this.setTime(dias);
+	}
+	
+	public void incrementa(int dias) {
+		long milisegundos = dias * 1000 * 60 * 24;
+		
+		this.incrementa(milisegundos);
+	}
+
+}
